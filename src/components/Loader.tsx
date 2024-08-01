aa335ba41fffb2ebcc705d4be069dceb47f6aedf
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-01 14:26:56 +0530

    added rotating pokeball as loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+const styles = {
+    animation: 'rotate 2s linear infinite',
+    width:'75px',
+    height:'75px'
+}
 
 export default function Loader() {
     return (
-        <div>
-      Loading
-        </div>
+        <>
+            <style>
+                {`
+                  @keyframes rotate {
+                    0%{
+                        transform: rotate(0deg);
+                      }
+                    100%{
+                        transform: rotate(360deg);
+                      }
+                    }
+              `}
+            </style>
+            <div className="flex items-center justify-center h-full">
+                <img src='/images/pokeball.png' className='' style={styles} />
+            </div>
+        </>
     )
 }
