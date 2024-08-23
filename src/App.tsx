e4a5cdb52ef12ef12b757db97a6d2b0d1e181d73
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-23 11:26:51 +0530

    minor css change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Routes from './routes/Routes'
 
 export default function App() {
     return (
-        <main className='md:container md:mx-auto'>
+        <main className='md:container md:mx-auto h-screen'>
             <Routes />
         </main>
     )
