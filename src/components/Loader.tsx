8b49b04680f3c840af143195a03c4cff55fdeefa
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-26 14:36:56 +0530

    minor loader fix

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -19,7 +19,7 @@ export default function Loader() {
                     }
               `}
             </style>
-            <div className="flex items-center justify-center h-full">
+            <div className="flex items-center justify-center h-full overflow-hidden">
                 <img src='/images/pokeball.png' className='' style={styles} />
             </div>
         </>
