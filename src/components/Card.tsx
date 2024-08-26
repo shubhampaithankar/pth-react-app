0ec6ef3e6bebd8dba3ee54e23b5e9752487ef273
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-26 14:44:43 +0530

    added total base stat

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,28 +3,30 @@ import Type from './Type'
 import { Pokemon } from '../utils/Types'
 
 export default function Card({ pokemon }: { pokemon: Pokemon }) {
+    const {sprites, name, abilities, id, stats, types} = pokemon
+    const total = stats.reduce((acc, curr) => acc + curr.base_stat, 0)
     return (
         <div className="w-[350px] max-h-[600px] relative">
             <div className="relative w-full p-7 shadow-lg rounded-lg bg-white">
                 <img
-                    src={pokemon.sprites.other['official-artwork'].front_default!}
-                    alt={pokemon.name}
+                    src={sprites.other['official-artwork'].front_default!}
+                    alt={name}
                     className="block w-[180px] max-h-[200px] mx-auto my-5"
-
                 />
                 <div className="hp w-[80px] bg-white text-center py-2 rounded-full ml-auto font-light">
-                    <span className="text-xs tracking-wider font-semibold">#{pokemon.id}</span>
+                    <span className="text-xs tracking-wider font-semibold">#{id}</span>
                 </div>
-                <h2 className="poke-name text-center font-semibold text-lg mt-3">{pokemon.name}</h2>
+                <h2 className="poke-name text-center font-semibold text-lg mt-3">{name}</h2>
                 <div className="types flex justify-around my-5">
-                    { pokemon.types.map((type, i) => 
+                    { types.map((type, i) => 
                         <Type key={i} type={type.type.name}/>
                     ) }
                 </div>
                 <div className="stats flex items-center justify-between text-center">
-                    { pokemon.stats.map(({ stat, base_stat }, i) => 
+                    { stats.map(({ stat, base_stat }, i) => 
                         <p key={i} className="text-[#404060]">{stat.name}: {base_stat}</p>
                     ) }
+                    <p className="text-[#404060]">Total: {total}</p>
                 </div>
                 <button
                     id="btn"
