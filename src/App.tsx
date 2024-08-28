45df6f4a120ea31838cf305efe03a51f5e05ad0e
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-28 14:33:50 +0530

    added suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { AuthProvider } from './hooks/useAuth'
 import { Navbar } from './components/common/'
 import Routes from './routes/Routes'
+import { Suspense } from 'react'
 
 export default function App() {
     return (
         <AuthProvider>
             <Navbar />
-            <main className='md:container md:mx-auto h-screen'>
-                <Routes />
-            </main>
+            <Suspense fallback={<>Loading page</>}>
+                <main className='md:container md:mx-auto h-screen'>
+                    <Routes />
+                </main>
+            </Suspense>
         </AuthProvider>
     )
 }
