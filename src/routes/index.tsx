3776cf4cf88a6a5c8f2844f83865e175014bada2
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-29 14:31:20 +0530

    routes fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,76 +1,73 @@
 import { Navigate, RouteObject } from 'react-router-dom'
-import { Auth, Home, Battle, Pokemon, Profile, BattleGround } from '../pages'
+import { Auth, Home, Battle, Pokemon, Profile, BattleGround, PokemonDetail } from '../pages'
 import PrivateRoute from './PrivateRoute'
+import { Outlet } from 'react-router-dom'
 
 export const AppRoutes: RouteObject[] = [
     {
         path: '/',
+        element: <Navigate to='home' />, // Redirect root path to home
+    },
+    {
+        path: 'home',
+        element: <Home />,
+    },
+    {
+        path: 'auth',
+        element: <Auth />,
+    },
+    {
+        path: 'profile',
+        element: (
+            <PrivateRoute>
+                <Outlet />
+            </PrivateRoute>
+        ),
         children: [
             {
-                path: '/',
-                element: <Navigate to='home' />
-            },
-            {
-                path: 'home',
-                element: <Home />
-            },
-            {
-                path: 'auth',
-                element: <Auth />
-            },
-            {
-                path: 'profile',
-                children: [
-                    {
-                        path: ':username', // username as a dynamic param
-                        element: (
-                            <PrivateRoute>
-                                <Profile />
-                            </PrivateRoute>
-                        )
-                    }
-                ]
-            },
-            {
-                path: 'pokemon',
-                element: (
-                    <PrivateRoute>
-                        <Pokemon />
-                    </PrivateRoute>
-                ),
-                children: [
-                    {
-                        path: ':id', // `id` as a dynamic param
-                        element: (
-                            <PrivateRoute>
-                                <Pokemon />
-                            </PrivateRoute>
-                        ),
-                    },
-                ]
-            },
-            {
-                path: 'battle',
-                element: (
-                    <PrivateRoute>
-                        <Battle />
-                    </PrivateRoute>
-                ),
-                children: [
-                    {
-                        path: ':roomID', // `roomID` as a dynamic param for WebSocket
-                        element: (
-                            <PrivateRoute>
-                                <BattleGround />
-                            </PrivateRoute>
-                        )
-                    }
-                ]
-            },
-            {
-                path: '*',
-                element: <>404 - Page not found</>
+                path: ':username',
+                element: <Profile />, // Profile page for a specific username
             }
-        ]
+        ],
+    },
+    {
+        path: 'pokemon',
+        element: (
+            <PrivateRoute>
+                <Outlet />
+            </PrivateRoute>
+        ),
+        children: [
+            {
+                path: '',
+                element: <Pokemon />, // Main Pokémon page
+            },
+            {
+                path: ':id',
+                element: <PokemonDetail />, // Pokémon detail page
+            },
+        ],
+    },
+    {
+        path: 'battle',
+        element: (
+            <PrivateRoute>
+                <Outlet />
+            </PrivateRoute>
+        ),
+        children: [
+            {
+                path: '',
+                element: <Battle />, // Main Battle page
+            },
+            {
+                path: ':roomID',
+                element: <BattleGround />, // Battle room page
+            },
+        ],
+    },
+    {
+        path: '*',
+        element: <>404 - Page not found</>, // Wildcard route for 404 errors
     }
 ]
