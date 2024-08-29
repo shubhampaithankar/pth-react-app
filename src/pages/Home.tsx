756ea0d3fd53f24791eeec62d3f847dfb47b9fb8
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-29 14:31:34 +0530

    home page remove navigation as its not needed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,9 @@
-import { useEffect } from 'react'
 import { useAuth } from '../hooks'
 import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
     const { isAuthenticated } = useAuth()
     const navigate = useNavigate()
-
-    useEffect(() => {
-        if (isAuthenticated) {
-            navigate('/pokemon')
-            return
-        }
-    }, [isAuthenticated, navigate])
 
     return (
         <section className='h-full flex items-center flex-wrap gap-2 justify-center'>
