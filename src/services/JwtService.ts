02ce2cec1623c24ace5be0ae799b1bdad5e04e34
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-03 13:15:18 +0530

    minor changes

--- src/services/JwtService.ts
+++ src/services/JwtService.ts
@@ -1,11 +1,10 @@
 import { jwtDecode } from 'jwt-decode'
 
-export const isTokenExpired = (token: string) => {
+export const isTokenExpired = (token: string | null) => {
     if (!token) return true
     try {
         const decodedToken = jwtDecode(token)
         const currentTime = Date.now() / 1000
-        console.log(decodedToken.exp ? decodedToken.exp < currentTime : true)
         return decodedToken.exp ? decodedToken.exp < currentTime : true
     } catch (error) {
         console.error('Error decoding token:', error)
