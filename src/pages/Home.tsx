3e976b23f6050f29c517e5e1d6f931b967fa8c5e
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-04 19:20:47 +0530

    minor changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,23 @@
 import { useAuth } from '../hooks'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Home() {
     const { isAuthenticated } = useAuth()
     const navigate = useNavigate()
 
+    // add proper tailwind css classes for buttons and the layout to make it fancy
     return (
-        <section className='h-full flex items-center flex-wrap gap-2 justify-center'>
+        <section className='h-full flex flex-col items-center flex-wrap gap-2 justify-center'>
             <h1>Welcome to pokemon trainer hub</h1>
+            { isAuthenticated? 
+                <Link to='/dashboard' className='btn btn-primary '>
+                    Go to dashboard
+                </Link>
+                :
+                <Link to='/auth' className='btn btn-primary'>
+                    Login
+                </Link>
+            }     
         </section>
     )
 }
