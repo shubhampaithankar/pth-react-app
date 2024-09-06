0a72c5a670555250978a1b16b8b594d4dff7bc03
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-06 14:58:17 +0530

    added react query package

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-
+  
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <QueryClientProvider client={new QueryClient()}>
+                <App />
+            </QueryClientProvider>
         </BrowserRouter>
     </React.StrictMode>,
 )
