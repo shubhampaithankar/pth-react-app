d56178480edb37e158e1eb6b889cac74aa9bc243
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-07 19:20:47 +0530

    using react query on random pokemon

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -3,31 +3,31 @@ import { useEffect, useState } from 'react'
 import { Pokemon as PokemonObj } from '../utils/Types'
 import { getRandomPokemon } from '../services/ApiService'
 import { Loader, Card } from '../components'
+import { useQuery } from '@tanstack/react-query'
+
+// todo implement useQuery, 
 
 export default function Pokemon() {
-    const [loading, setLoading] = useState(false)
     const [pokemon, setPokemon] = useState<PokemonObj[]>([])
 
-    const fetchPokemon = async () => {
-        try {
-            setLoading(true)
-            const { result, error, ack } = await getRandomPokemon()
-            if (ack === 1) setPokemon(result)
-            else throw error
-        } catch (error) {
-            
-        }
-        setLoading(false)
-    }
+    const { data, isLoading, isSuccess, refetch } = useQuery({
+        queryKey: ['fetchRandomPokemon'],
+        queryFn: getRandomPokemon,
+    })
 
+    // todo: populate pokemon array with data fetched with useQuery
     useEffect(() => {
-        if (pokemon.length === 0) fetchPokemon()
-    }, [])
+        if (data) {
+            const { ack, error, pokemon } = data
+            if (ack === 1) setPokemon(pokemon!)
+            else console.log(error)
+        }
+    }, [data])
     
     return (
         <section className='h-full flex items-center flex-wrap gap-2 justify-center'>
-            { loading && <Loader /> }
-            { !loading && pokemon.map((entry, i) => <Card key={i} pokemon={entry}/>) }
+            { isLoading && <Loader /> }
+            { (!isLoading && isSuccess) && pokemon.map((entry, i) => <Card key={i} pokemon={entry}/>) }
         </section>
     )
 }
