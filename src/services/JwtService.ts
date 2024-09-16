73d28e1dd17303ce426cdd7be86d817c7c99bf6b
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-16 14:10:07 +0530

    implemented tryCatch in jwt service

--- src/services/JwtService.ts
+++ src/services/JwtService.ts
@@ -1,13 +1,19 @@
 import { jwtDecode } from 'jwt-decode'
+import { tryCatch } from '../utils/HelperFunctions'
 
-export const isTokenExpired = (token: string | null) => {
-    if (!token) return true
-    try {
-        const decodedToken = jwtDecode(token)
+export const isTokenExpired = (token: string | null): boolean => {
+    if (!token) return true // No token means it's expired
+
+    const [error, decoded] = tryCatch(() => {
+        const decodedToken = jwtDecode<{ exp: number | undefined }>(token) // Specify the decoded token shape
         const currentTime = Date.now() / 1000
         return decodedToken.exp ? decodedToken.exp < currentTime : true
-    } catch (error) {
-        console.error('Error decoding token:', error)
-        return true
+    })
+
+    if (error) {
+        console.error('Error decoding token:', error) // Log error for debugging
+        return true // If there is an error, consider the token expired
     }
+
+    return !!decoded // If the decoded result is true, the token is still valid
 }
